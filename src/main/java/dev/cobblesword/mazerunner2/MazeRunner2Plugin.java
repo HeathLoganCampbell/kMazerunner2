b741fdaa03cef7574881e91c276491f2aa0130b9
HeathLoganCampbell <Heath.campbell2@datacom.co.nz>
2025-02-08 00:07:36 +1100

    init

---
+++ src/main/java/dev/cobblesword/mazerunner2/MazeRunner2Plugin.java
@@ -0,0 +1,12 @@
+package dev.cobblesword.mazerunner2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MazeRunner2Plugin extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        System.out.println("MazeRunner2 Plugin enabled");
+    }
+}
