359c6bbb4d4edacf2ed6638e3fabacd79007cf16
HeathLoganCampbell <Heath.campbell2@datacom.co.nz>
2025-02-08 22:41:11 +1100

    speed up block placement

--- src/main/java/dev/cobblesword/mazerunner2/world/GameWorld.java
+++ src/main/java/dev/cobblesword/mazerunner2/world/GameWorld.java
@@ -4,6 +4,7 @@ import dev.cobblesword.mazerunner2.map.Map;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 
+import java.util.Random;
 import java.util.UUID;
 
 import static dev.cobblesword.mazerunner2.maze.BasicMazeGenerator.*;
@@ -19,14 +20,19 @@ public class GameWorld
         this.seed = seed;
         this.map = new Map(this.seed, 12, 12);
         this.map.generateMap();
+        Random random = new Random(seed);
 
         WorldCreator creator = new WorldCreator("GW-" + UUID.randomUUID());
-        creator.generator(new VoidChunkGenerator()); // Use custom generator
+        creator.generator(new VoidChunkGenerator(this.map)); // Use custom generator
         this.world = Bukkit.createWorld(creator);
         this.world.setAutoSave(false);
+        this.world.setDifficulty(Difficulty.NORMAL);
+//        this.world.setGameRuleValue("doDaylightCycle", "false");
+        int wallHeight = 10;
 
         int scalar = 3;
         for (int x = 0; x < this.map.getTotalWidth(); x++) {
+            System.out.println("Maze Generated " + Math.round(((float) x / this.map.getTotalWidth()) * 100) + "%");
             for (int z = 0; z < this.map.getTotalHeight(); z++) {
                 int sample = this.map.getSample(x, z);
                 if(sample == PATH || sample == DEADEND)
@@ -35,13 +41,15 @@ public class GameWorld
                         for (int dz = 0; dz < scalar; dz++) {
                             int newX = x * scalar + dx;
                             int newZ = z * scalar + dz;
-                            this.world.getBlockAt(newX, 1, newZ).setBlockData(Material.STONE.createBlockData(), false);
+                            this.world.getBlockAt(newX, 1, newZ).setType(Material.STONE, false);
                         }
                     }
 
                     if(sample == DEADEND)
                     {
-                        this.world.getBlockAt(x * scalar + 1, 2, z * scalar + 1).setBlockData(Material.CHEST.createBlockData(), false);
+                        if (random.nextInt(5) == 1) {
+                            this.world.getBlockAt(x * scalar + 1, 2, z * scalar + 1).setType(Material.CHEST, false);
+                        }
                     }
                 }
 
@@ -51,11 +59,14 @@ public class GameWorld
                         for (int dz = 0; dz < scalar; dz++) {
                             int newX = x * scalar + dx;
                             int newZ = z * scalar + dz;
-                            this.world.getBlockAt(newX, 1, newZ).setBlockData(Material.STONE.createBlockData(), false);
-                            this.world.getBlockAt(newX, 2, newZ).setBlockData(Material.STONE.createBlockData(), false);
-                            this.world.getBlockAt(newX, 3, newZ).setBlockData(Material.STONE.createBlockData(), false);
-                            this.world.getBlockAt(newX, 4, newZ).setBlockData(Material.STONE.createBlockData(), false);
-                            this.world.getBlockAt(newX, 5, newZ).setBlockData(Material.STONE.createBlockData(), false);
+                            for (int i = 0; i < wallHeight; i++) {
+                                Material material = Material.STONE_BRICKS;
+                                if (random.nextInt(3) == 1) {
+                                    material = Material.CRACKED_STONE_BRICKS;
+                                }
+
+                                this.world.getBlockAt(newX, i, newZ).setType(material, false);
+                            }
                         }
                     }
                 }
@@ -66,7 +77,7 @@ public class GameWorld
                         for (int dz = 0; dz < scalar; dz++) {
                             int newX = x * scalar + dx;
                             int newZ = z * scalar + dz;
-                            this.world.getBlockAt(newX, 1, newZ).setBlockData(Material.GRASS_BLOCK.createBlockData(), false);
+                            this.world.getBlockAt(newX, 1, newZ).setType(Material.GRASS_BLOCK, false);
                         }
                     }
                 }
--- src/main/java/dev/cobblesword/mazerunner2/world/VoidChunkGenerator.java
+++ src/main/java/dev/cobblesword/mazerunner2/world/VoidChunkGenerator.java
@@ -1,13 +1,22 @@
 package dev.cobblesword.mazerunner2.world;
 
+import dev.cobblesword.mazerunner2.map.Map;
 import org.bukkit.World;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.generator.WorldInfo;
 import java.util.Random;
 
 public class VoidChunkGenerator extends ChunkGenerator {
+    private Map map;
+
+    public VoidChunkGenerator(Map map)
+    {
+        this.map = map;
+    }
+
     @Override
-    public void generateNoise(WorldInfo worldInfo, Random random, int chunkX, int chunkZ, ChunkData chunkData) {
+    public void generateNoise(WorldInfo worldInfo, Random random, int chunkX, int chunkZ, ChunkData chunkData)
+    {
         // Do nothing, as we want an empty void world
     }
 }
